88c174b89e9397377a0e18ca9e34acd4f644187f
king <117745900@qq.com>
2021-09-15 10:59:43 +0800

    fix: rm .umi

--- src/renderer/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/king/Desktop/github/orbit-dev/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu',],
-});
-
-export { plugin };
